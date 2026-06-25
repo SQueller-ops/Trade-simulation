c1c9d2826e218bc23f742bd4591591519594c30f
SQueller-ops <SQueller@forteprep.org>
2026-06-25 08:32:39 -0400

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -5,11 +5,11 @@
 //  Paste each value between the quotes, replacing the PASTE_... text.
 // ============================================================
 window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyAr-M-wts8q-GcWl7wpGKqRi8N0Gv1Dmfw",
+  authDomain: "trade-simulation-560fb.firebaseapp.com",
+  databaseURL: https://trade-simulation-560fb-default-rtdb.firebaseio.com/
+  projectId: "trade-simulation-560fb",
+  storageBucket: "trade-simulation-560fb.firebasestorage.app",
+  messagingSenderId: "494814536633",
+  appId: "1:494814536633:web:286726d8ffcc5d6f884c68",
 };
